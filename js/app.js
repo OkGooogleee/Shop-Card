c21a41a4f8b82d03d53dd0c01031f4fe36aeb2d8
OkGooogleee <krot.pipi@mail.ru>
2020-09-22 01:22:41 +0300

    second step

---
+++ js/app.js
@@ -0,0 +1,18 @@
+(function () {
+
+  var menuData = {
+    "00000012": {
+      productName: "Mouse",
+      price: 17.90
+    },
+    "00000042": {
+      productName: "AMD CPU",
+      price: 79.99
+    },
+    "000000057": {
+      productName: "Keyboard",
+      price: 19.99
+    }
+  }
+
+})();
